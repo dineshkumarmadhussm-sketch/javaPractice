2fecbab4c7fb654d548b4808b18d16f4fbb11f24
dineshkumarmadhussm-sketch <dineshkumarmadhussm@gmail.com>
2026-05-07 17:32:04 +0530

    Arguments in java

---
+++ src/methods/arguments.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class arguments {
+    static void sum(int a,int b ,int c ) {   //parameters
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+
+    static void main(String[] args) {
+        sum(10, 20,30);    // Arguments
+
+    }
+}
