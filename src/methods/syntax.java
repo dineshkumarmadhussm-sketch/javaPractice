cc6d97e594580d546f15dea07a90e5fbecfeb728
dineshkumarmadhussm-sketch <dineshkumarmadhussm@gmail.com>
2026-05-07 17:20:44 +0530

    Initial commit

---
+++ src/methods/syntax.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class syntax {
+ public   static void shravan() {
+        karan();
+        System.out.println("khushi");
+    }
+   public static void main(String[] args) {
+    riyanshi();
+    }
+
+   public  static void karan() {
+        System.out.println("yashika");
+    }
+
+   public  static void riyanshi() {
+        System.out.println("ajay");
+        shravan();
+    }
+}
